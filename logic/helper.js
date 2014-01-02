6e92656d47808511a622fc7bef91b56354504876
AntonNarkevich <anton.narkevich@wsa-205.icx.local>
2014-01-02 13:45:15 +0300

    JS pages are being registered in clientMap.js

---
+++ logic/helper.js
@@ -0,0 +1,77 @@
+"use strict";
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+/**
+ * Capitalizes first letter newPage => NewPage.
+ * @param string String to capitalize.
+ * @returns {string} Capitalized string.
+ */
+function capitalizeFirstLetter(string) {
+	logger.trace('capitalizeFirstLetter: ' + string);
+
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ * Gets js callback name by page name newPage => runNewPage.
+ * @param pageName Name of the page.
+ * @returns {string} Callback function name.
+ */
+function getJsCallbackName(pageName) {
+	logger.trace('getJsCallbackName: ' + pageName);
+
+	return 'run' + capitalizeFirstLetter(pageName);
+}
+
+/**
+ * Generates callback name
+ * and object literal, inserts it to the specific place in clientMap.js.
+ * @param pageName Page to register.
+ * @param clientMapString clientMap.js string content.
+ * @returns {newClientMapString} Processed clientMap.js content
+ */
+function registerJsFile(pageName, clientMapString) {
+	var jsArrayPattern, registrationString, callbackName, newClientMapString;
+
+	/**
+	 * Pattern of the last object in array
+	 * Warning: nested object are not supported.
+	 * @type {RegExp}
+	 */
+	jsArrayPattern = new RegExp(
+			'(js\\s*:\\s*\\[' +	//Starts with js: [
+			'[\\s\\S]*?' +	//Array content, any symbols.
+			'(\\s*)' +	//Getting whites before the last element to keep the aligning.
+			'(?:{[^{]*?}))' +	//Object literal without nested objects.
+			'(?:[^,])'	//	Doesn't have "," in the end. So it's the last in array.
+	);
+
+	callbackName = getJsCallbackName(pageName);
+
+	logger.trace('registerJsFile: callbackName is ' + callbackName);
+
+	registrationString = "{ src: 'pageName.js', contentCondition: ['#SomeUniqueElementIdFromThePage'], callback: 'callBackName'}"
+		.replace('pageName', pageName)
+		.replace('callBackName', callbackName);
+
+	logger.trace('registerJsFile: registrationString is ' + registrationString);
+
+	//Replace with: old content + spacing + reg. string
+	newClientMapString = clientMapString.replace(jsArrayPattern, '$1,$2' + registrationString);
+
+	logger.info('registerJsFile: insertion is completed:\n' + newClientMapString);
+
+	return newClientMapString;
+}
+
+function registerCssFile() {
+	return;
+}
+
+module.exports = {
+	registerJsFile: registerJsFile,
+	registerCssFile: registerCssFile,
+	capitalizeFirstLetter: capitalizeFirstLetter,
+	getJsCallbackName: getJsCallbackName
+};
--- page/index.js
+++ page/index.js
@@ -2,6 +2,9 @@
 var util = require('util');
 var yeoman = require('yeoman-generator');
 var fs = require('fs');
+var helper = require('../logic/helper');
+var log4js = require('log4js');
+var logger = log4js.getLogger();
 
 var PageGenerator = module.exports = function PageGenerator(args, options, config) {
 	// By calling `NamedBase` here, we get the argument to the subgenerator call
@@ -11,11 +14,10 @@ var PageGenerator = module.exports = function PageGenerator(args, options, confi
 	console.log('You called the page subgenerator with the argument ' + this.name + '.');
 
 	//Reading config file. Should be written by main generator.
-	var respConfig;
+	var respConfig, content;
 
 	try {
-		var content = fs.readFileSync('respConfig.json', 'utf8');
-		console.log(content);
+		content = fs.readFileSync('respConfig.json', 'utf8');
 		respConfig = JSON.parse(content);
 	}
 	catch (exception) {
@@ -33,12 +35,40 @@ var PageGenerator = module.exports = function PageGenerator(args, options, confi
 util.inherits(PageGenerator, yeoman.generators.NamedBase);
 
 PageGenerator.prototype.files = function files() {
-	this.capitaliseFirstLetter = function capitaliseFirstLetter(string)
-	{
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	};
+	var that = this;
 
-	this.template('page.js', 'js/' + this.name + '.js');
-	this.template('page.less', 'less/' + this.name + '.less');
+	logger.trace('PageGenerator.prototype.files: name is ' + that.name);
+
+	this.callBackName = helper.getJsCallbackName(this.name);
+
+	logger.trace('PageGenerator.prototype.files: callBackName is ' + this.callBackName);
+
+	this.template('page.js', 'sources/js/' + this.name + '.js');
+	this.template('page.less', 'sources/less/' + this.name + '.less');
+
+	var clientMapFileName = 'sources/js/clientMap.js';
+
+	//Registration in the clientMap.js
+	fs.readFile(clientMapFileName, 'utf8', function (err, data) {
+		var newClientMapString;
+
+		logger.trace('PageGenerator.prototype.files: clentMap data is:\n' + data);
+
+		if (err) {
+			logger.error('Couldn\'t read clientMap.js to register new page.');
+			throw err;
+		}
+
+		newClientMapString = helper.registerJsFile(that.name, data);
+
+		logger.trace('PageGenerator.prototype.files: writing to ' + clientMapFileName);
+
+		fs.writeFile(clientMapFileName, newClientMapString, 'utf8', function (err) {
+			if (err) {
+				logger.error('Couldn\'t write to clientMap.js to register new page: ' + err);
+				throw err;
+			}
+		});
+	});
 };
 
